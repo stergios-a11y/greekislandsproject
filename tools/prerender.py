866e1763d644dd96630c9ffb89cda3797a68c868
Stergios Gousios <stergiosgousios@gmail.com>
2026-05-07 17:28:13 +0300

    update

--- tools/prerender.py
+++ tools/prerender.py
@@ -1050,6 +1050,7 @@ def render_page(key, data, meta, lang='en'):
 <link rel="icon" href="{asset_prefix}favicon.ico" sizes="any">
 <link rel="icon" href="{asset_prefix}favicon.svg" type="image/svg+xml">
 <link rel="icon" type="image/png" sizes="32x32" href="{asset_prefix}favicon-32.png">
+<link rel="icon" type="image/png" sizes="96x96" href="{asset_prefix}favicon-96.png">
 <link rel="icon" type="image/png" sizes="16x16" href="{asset_prefix}favicon-16.png">
 <link rel="apple-touch-icon" href="{asset_prefix}apple-touch-icon.png">
 <meta name="apple-mobile-web-app-title" content="Aegean Blueprint">
@@ -1466,6 +1467,7 @@ def generate_festivals_page(island_keys):
             '<link rel="icon" href="/favicon.ico" sizes="any">\n'
             '<link rel="icon" href="/favicon.svg" type="image/svg+xml">\n'
             '<link rel="icon" type="image/png" sizes="32x32" href="/favicon-32.png">\n'
+            '<link rel="icon" type="image/png" sizes="96x96" href="/favicon-96.png">\n'
             '<link rel="icon" type="image/png" sizes="16x16" href="/favicon-16.png">\n'
             '<link rel="apple-touch-icon" href="/apple-touch-icon.png">\n'
             '<meta name="apple-mobile-web-app-title" content="Aegean Blueprint">\n'
