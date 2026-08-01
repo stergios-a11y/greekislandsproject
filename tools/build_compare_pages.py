5fbc0e36833a23875fb0285a60b1b8772eac42ff
stergios-a11y <stergiosgousios@gmail.com>
2026-08-02 00:47:58 +0300

    Fix mobile pill overflow hiding the Filters button; Naxos photos; dark mode follows OS

--- tools/build_compare_pages.py
+++ tools/build_compare_pages.py
@@ -48,7 +48,7 @@ def _resolve_root():
 
 ROOT = _resolve_root()
 SITE_URL = 'https://aegeanblueprint.com'
-ASSET_V = 85
+ASSET_V = 87
 
 from datetime import date as _date
 YEAR = _date.today().year
